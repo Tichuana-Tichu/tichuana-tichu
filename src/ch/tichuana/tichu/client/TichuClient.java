c31aee7db31e8d35c00a02a28df5711e963df2a6
philipp <phipu@windowslive.com>
2019-09-25 13:39:18 +0200

    implemented JavaFX application

---
+++ src/ch/tichuana/tichu/client/TichuClient.java
@@ -0,0 +1,15 @@
+package ch.tichuana.tichu.client;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Client extends Application {
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+}
