c1b14ea1952550b2decdc60e389a2bdf11208669
sekthor <sysdroyed@gmail.com>
2019-09-30 11:16:28 +0200

    File-name and class-name did not match

--- src/ch/tichuana/tichu/client/TichuClient.java
+++ src/ch/tichuana/tichu/client/TichuClient.java
@@ -3,7 +3,7 @@ package ch.tichuana.tichu.client;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Client extends Application {
+public class TichuClient extends Application {
     public static void main(String[] args){
         launch(args);
     }
