1f5ab1999eeaab15f97caa6cf2e1b8c823d7f98b
sekthor <sysdroyed@gmail.com>
2019-10-21 17:10:15 +0200

    added singleton pattern to ServiceLocator, added initialize() method

--- src/ch/tichuana/tichu/client/Client.java
+++ src/ch/tichuana/tichu/client/Client.java
@@ -2,9 +2,15 @@ package ch.tichuana.tichu.client;
 
 import ch.tichuana.tichu.client.model.ClientModel;
 import ch.tichuana.tichu.client.controller.*;
+import ch.tichuana.tichu.client.services.ServiceLocator;
 import ch.tichuana.tichu.client.view.*;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Client extends Application {
 
@@ -28,5 +34,18 @@ public class Client extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
+	}
+
+	public static void initalize(){
+		ServiceLocator serviceLocator = ServiceLocator.getServiceLocator();
+
+		// initialize properites from file
+		Properties properties = new Properties();
+		try (InputStream input = new FileInputStream("../resources/config.properties")) {
+			properties.load(input);
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+		serviceLocator.setProperties(properties);
 	}
 }
--- src/ch/tichuana/tichu/client/services/ServiceLocator.java
+++ src/ch/tichuana/tichu/client/services/ServiceLocator.java
@@ -10,14 +10,39 @@ public class ServiceLocator {
 	private static ServiceLocator serviceLocator;
 	private Translator translator;
 	private Properties properties;
-	private Locale[] locales;
+	private Locale[] locales; // = {new Locale("de"), new Locale("de")};
 
+	/**
+	 * ServiceLocator is a singleton. There's only one instance.
+	 * Private Constructor only used by factory method.
+	 * @author Christian
+	 */
 	private ServiceLocator() {
-		// TODO - implement ServiceLocator.ServiceLocator
+		// No need for code, only needs to be private
 	}
 
+	/**
+	 * Factory method returns ServiceLocator if it already exists. Will create it, if not.
+	 * @return serviceLocator
+	 */
 	public static ServiceLocator getServiceLocator() {
+		if (serviceLocator == null){
+			serviceLocator = new ServiceLocator();
+		}
 		return serviceLocator;
 	}
 
+	// getters and setters
+	public Translator getTranslator() {
+		return translator;
+	}
+	public void setTranslator(Translator translator) {
+		this.translator = translator;
+	}
+	public Properties getProperties() {
+		return properties;
+	}
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
 }
