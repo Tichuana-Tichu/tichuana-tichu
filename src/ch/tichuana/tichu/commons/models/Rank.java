f04daa8f0603956fdd14225d3874c2bbe3dfc1a7
Cyykra <dominik.spillmann@students.fhnw.ch>
2019-10-28 11:00:19 +0100

    all png files added to resources

--- src/ch/tichuana/tichu/commons/models/Rank.java
+++ src/ch/tichuana/tichu/commons/models/Rank.java
@@ -4,7 +4,7 @@ public enum Rank {
 	dog, majhong, two, three, four, five, six, seven, eight, nine, ten, Jack, Queen, King, Ace, phoenix, dragon;
 
 	public String toString() {
-		// TODO - implement Rank.toString
+			// TODO - implement Rank.toString
 		return null;
 	}
 
