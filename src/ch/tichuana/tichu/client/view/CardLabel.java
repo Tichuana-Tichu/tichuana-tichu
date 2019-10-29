ee21307b1757fe42c618f55506f975702cfcf307
philipp <phipu@windowslive.com>
2019-10-29 14:20:24 +0100

    basic init 2

--- src/ch/tichuana/tichu/client/view/CardLabel.java
+++ src/ch/tichuana/tichu/client/view/CardLabel.java
@@ -1,23 +1,41 @@
 package ch.tichuana.tichu.client.view;
 
+import ch.tichuana.tichu.commons.models.Card;
+import ch.tichuana.tichu.commons.models.Suit;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class Card {
 
-	private Image card;
-	private ImageView cardView;
 
-	public Card() {
-		// TODO - implement Card.Card
-	}
+public class CardLabel extends Label {
 
-	public ImageView getCardView() {
-		return this.cardView;
-	}
+    private Image cardimg;
+    private ImageView cardView;
+    private Card card;
 
-	private String cardToFilename(){
-		
-	}
+    public CardLabel(Card card) {
+        super();
+        this.card = card;
+    }
+
+    public ImageView getCardView() {
+        return this.cardView;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public String cardToFilename(Card card){
+        String fileName = "";
+        Suit suit = card.getSuit();
+        if (suit != null){
+            fileName += suit.toString();
+        }
+        fileName += card.getRank().toString();
+        fileName += ".png";
+        return fileName;
+    }
 
 }
