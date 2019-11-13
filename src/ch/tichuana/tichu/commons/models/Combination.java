eab7a4277f1984c4e32d20df47e480ffb51b622a
philipp <phipu@windowslive.com>
2019-11-13 15:18:06 +0100

    refactoring Combination.java in a workshop

--- src/ch/tichuana/tichu/commons/models/Combination.java
+++ src/ch/tichuana/tichu/commons/models/Combination.java
@@ -4,18 +4,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public enum Combination {
-	HighCard, OnePair, ThreeOfAKind, Steps, FullHouse, Straight, FourOfAKindBomb, FourOfAKindPhoenix, StraightFlushBomb;
-
-	Combination() {
-	}
-
+	HighCard, OnePair, ThreeOfAKind, Steps, FullHouse, Straight,
+	FourOfAKindBomb, FourOfAKindPhoenix, StraightFlushBomb;
 
 	public static ArrayList<Card> clonedCards;
+
 	/**
 	 * All Combinations are going to be checked and returned by currentEval.
-	 * @autor
-	 * @return
-	 * @param cards
+	 *
+	 * @param cards ArrayList including selected cards from client view
+	 * @return combination type
+	 * @author dominik
 	 */
 	public static Combination evaluateCombination(ArrayList<Card> cards) {
 		Combination currentEval = HighCard;
@@ -23,29 +22,29 @@ public enum Combination {
 		clonedCards = (ArrayList<Card>) cards.clone();
 		Collections.sort(clonedCards);
 
-		if(isOnePair(clonedCards)) currentEval = OnePair;
-		if(isThreeOfAKind(clonedCards)) currentEval = ThreeOfAKind;
-		if(isSteps(clonedCards)) currentEval = Steps;
-		if(isFullHouse(clonedCards)) currentEval = FullHouse;
-		if(isStraight(clonedCards)) currentEval = Straight;
-		if(isFourOfAKindBomb(clonedCards)) currentEval = FourOfAKindBomb;
-		if(isFourOfAKindPhoenix(clonedCards)) currentEval = FourOfAKindPhoenix;
-		if(isStraightFlushBomb(clonedCards)) currentEval = StraightFlushBomb;
-
+		if (isOnePair(clonedCards)) currentEval = OnePair;
+		if (isThreeOfAKind(clonedCards)) currentEval = ThreeOfAKind;
+		if (isSteps(clonedCards)) currentEval = Steps;
+		if (isFullHouse(clonedCards)) currentEval = FullHouse;
+		if (isStraight(clonedCards)) currentEval = Straight;
+		if (isFourOfAKindBomb(clonedCards)) currentEval = FourOfAKindBomb;
+		if (isFourOfAKindPhoenix(clonedCards)) currentEval = FourOfAKindPhoenix;
+		if (isStraightFlushBomb(clonedCards)) currentEval = StraightFlushBomb;
 
 		return currentEval;
 	}
 
-
 	/**
-	 * Before evaluating every possible combination, we check if the card phoenix is used.
-	 * @author
+	 * checks if cards contain phoenix
+	 *
 	 * @param cards
 	 * @return
+	 * @author dominik
 	 */
+	private static boolean containsPhoenix(ArrayList<Card> cards) {
+		return cards.contains(new Card(Rank.phoenix));
 
-	public static boolean containsPhoenix(ArrayList<Card> cards){
-		boolean containsPhoenix = false;
+	/*	boolean containsPhoenix = false;
 
 		for (int i = 0; i < cards.size() - 1; i++){
 			if(cards.get(i).getRank().ordinal() == 13){
@@ -53,44 +52,76 @@ public enum Combination {
 			}
 		}
 		return containsPhoenix;
+	*/
+	}
+
+
+	/**
+	 * checks if cards contain dog
+	 *
+	 * @param cards
+	 * @return
+	 * @author dominik
+	 */
+	private static boolean containsDog(ArrayList<Card> cards) {
+		return cards.contains(new Card(Rank.dog));
 	}
 
 	/**
-	 * Check if two cards in the Array have the same rank.
-	 * Phoenix control implemented.
-	 * @autor
-	 * @return
+	 *
 	 * @param cards
+	 * @return
+	 */
+	private static boolean containsMahjong(ArrayList<Card> cards) {
+		return false;
+	}
+
+	/**
+	 *
+	 * @param cards
+	 * @return
+	 */
+	private static boolean containsDragon(ArrayList<Card> cards) {
+		return false;
+	}
+
+	/**
+	 * checks if cards contain two cards of the same rank
+	 * or a phoenix and a random second card
+	 *
+	 * @param cards
+	 * @return
+	 * @author dominik
 	 */
 	public static boolean isOnePair(ArrayList<Card> cards) {
-		boolean phoenix = Combination.containsPhoenix(cards);
-		boolean found = false;
 
-		// if phoenix is included we just need to know, how many cards there are for onePair. if there are just 2
-		// cards, the pair is automatically given.
-		if (phoenix == true){
-			if (cards.size()== 2) found = true;
-
-		}else{
-			for (int i = 0; i < cards.size() - 1 && !found; i++) {
-				for (int j = i + 1; j < cards.size() && !found; j++) {
-					if (cards.get(i).getRank() == cards.get(j).getRank()) {
-						found = true;
-					}
-				}
-			}
-		}
-		return found;
+		if (cards.size() == 2 && !containsDog(cards)) {
+			return cards.get(0).getRank() == cards.get(1).getRank() || containsPhoenix(cards);
+		} else
+			return false;
 	}
 
 	/**
 	 * Check if three cards in the Array have the same rank.
 	 * Phoenix control implemented.
-	 * @autor
-	 * @return
+	 *
 	 * @param cards
+	 * @return
+	 * @author dominik
 	 */
 	public static boolean isThreeOfAKind(ArrayList<Card> cards) {
+
+		if (cards.size() == 3 && !containsDog(cards)) {
+			// 3 equal cards
+			return (cards.get(0).getRank() == cards.get(1).getRank() &&
+					cards.get(1).getRank() == cards.get(2).getRank()) ||
+					// or 2 equals cards and a phoenix
+					(cards.get(0).getRank() == cards.get(1).getRank() &&
+							containsPhoenix(cards));
+		} else
+			return false;
+	}
+		/*
 		boolean phoenix = Combination.containsPhoenix(cards);
 		boolean found = false;
 		// if two cards have the same rank and the third card is ordinal 13 --> phoenix, the combination is true.
@@ -117,26 +148,64 @@ public enum Combination {
 					}
 				}
 		return found;
-	}
+		 */
 
 	/**
 	 * Check if there are 4 or more cards in the array, if not return false. If there are, check if they are pairs and
 	 * the rank is one higher then the first pair.
-	 * @author
-	 * @return
+	 *
 	 * @param cards
+	 * @return
+	 * @author christian, dominik, philipp
 	 */
 	public static boolean isSteps(ArrayList<Card> cards) {
+		boolean phoenixUsed = false;
+
+		if (cards.size() % 2 == 0 && cards.size() > 3) {
+
+			//checks if a real step is given
+			for (int i = 0; i < cards.size()-1; i++) {
+				if (!(cards.get(i).getRank().ordinal()+1 == cards.get(i+2).getRank().ordinal()))
+					return false;
+			}
+
+			if (containsPhoenix(cards)) {//complex handling with phoenix
+
+				clonedCards = (ArrayList<Card>) cards.clone();
+				clonedCards.remove(clonedCards.get(clonedCards.size()-1));
+
+				for (int i = 1; i < cards.size()-1; i++) {
+					if (clonedCards.get(i).getRank() != clonedCards.get(i - 1).getRank() &&
+							clonedCards.get(i).getRank() != clonedCards.get(i + 1).getRank()) {
+						if (phoenixUsed) {
+							return false;
+						}
+						else {
+							phoenixUsed = true;
+						}
+					}
+				}
+
+			} else { //simple handling without phoenix
+				for (int i = 0; i < cards.size(); i += 2) {
+					if (cards.get(i).getRank() != cards.get(i + 1).getRank()) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+			/*
 		ArrayList<Card> clonedCards = (ArrayList<Card>) cards.clone();
 
 		boolean phoenix = Combination.containsPhoenix(cards);
 		boolean stepsFound = false;
 
-		if (phoenix == true){
+		if (phoenix){
 
-
-
-		}else {
+		} else {
 
 			switch (clonedCards.size()) {
 				case 1:
@@ -169,60 +238,55 @@ public enum Combination {
 			}
 		}
 		return stepsFound;
-
-
-	}
+		 */
 
 	/**
 	 * The first 3 cards and the second 2, or the first 2 and the following 3 cards need to be the same.
 	 * Phoenix control implemented.
-	 * @author
+	 * @author dominik
 	 * @return
 	 * @param cards
 	 */
 	public static boolean isFullHouse(ArrayList<Card> cards) {
-		boolean phoenix = Combination.containsPhoenix(cards);
-		boolean fullHouseFound = false;
 
-		if (phoenix == true){
-			//if first two cards have the same rank and card 3 & four have the same rank, fullHouse is given with phoenix attached.
-			if (cards.get(0).getRank().ordinal() == cards.get(1).getRank().ordinal() &&
-			cards.get(2).getRank().ordinal() == cards.get(3).getRank().ordinal() && cards.get(4).getRank().ordinal() == 13
-			//if card 2,3,4 have the same rank, fullhouse is automatically given.
-			|| cards.get(1).getRank().ordinal() == cards.get(2).getRank().ordinal() &&
-			cards.get(2).getRank().ordinal() == cards.get(3).getRank().ordinal()){
-				fullHouseFound = true;
+		//complex handling with phoenix
+		if (cards.size() == 5 && !containsDog(cards) && !containsDragon(cards) && !containsMahjong(cards)) {
+
+			if (containsPhoenix(cards)) {
+
+				if (cards.get(0).getRank() == cards.get(2).getRank() ||
+					cards.get(1).getRank() == cards.get(3).getRank() ||
+					(cards.get(0).getRank() == cards.get(1).getRank() &&
+					cards.get(2).getRank() == cards.get(3).getRank())) {
+					return true;
+				} else
+					return false;
+
+			} else {//simple handling without phoenix
+				return (cards.get(0).getRank() == cards.get(1).getRank() &&
+						cards.get(2).getRank() == cards.get(4).getRank()) ||
+						cards.get(0).getRank() == cards.get(2).getRank() &&
+						cards.get(3).getRank() == cards.get(4).getRank();
 			}
-		}else {
-			//first three cards must be equal & last two cards must be equal
-			if (cards.get(0).getRank().ordinal() == cards.get(1).getRank().ordinal() &&
-					cards.get(2).getRank().ordinal() == cards.get(3).getRank().ordinal() &&
-					cards.get(3).getRank().ordinal() == cards.get(4).getRank().ordinal()
-					||
-					//or first two cards must be equal & last three cards must be equal
-					cards.get(0).getRank().ordinal() == cards.get(1).getRank().ordinal() &&
-							cards.get(1).getRank().ordinal() == cards.get(2).getRank().ordinal() &&
-							cards.get(3).getRank().ordinal() == cards.get(4).getRank().ordinal()) {
-				fullHouseFound = true;
-			}
-		}
-		return fullHouseFound;
+		} else
+			return false;
 	}
 
 	/**
 	 * Check if every following Card is one rank higher then the one before.
-	 * @author
+	 * @author dominik
 	 * @return
 	 * @param cards
 	 */
 	public static boolean isStraight(ArrayList<Card> cards) {
-		boolean straightFound = true;
+		boolean found = true;
+		boolean phoenixUsed = false;
 
-		for(int i = 0; i < cards.size()-1 && straightFound; i++) {
+		for(int i = 0; i < cards.size()-1 && found; i++) {
 			if(cards.get(i).compareTo(cards.get(i+1)) != -1)
-				straightFound = false;
+				found = false;
 		}
-		return straightFound;
+		return found;
 	}
 
 	/**
@@ -248,7 +312,7 @@ public enum Combination {
 	 * @param cards
 	 * @return
 	 */
-	public static boolean isFourOfAKindPhoenix(ArrayList<Card> cards){
+	public static boolean isFourOfAKindPhoenix(ArrayList<Card> cards) {
 		boolean found = false;
 		if ((((cards.get(0).getRank().ordinal() == cards.get(3).getRank().ordinal()) &&
 				(cards.get(0).getRank().ordinal() == 13)) || (cards.get(3).getRank().ordinal() == 13)) ||
@@ -279,5 +343,4 @@ public enum Combination {
 		}
 		return straightFlushBomb;
 	}
-
 }
