1e8bb95d8ae4ae9fa94e4267e478209bab79c835
Philipp Hartmann <phipu@windowslive.com>
2019-11-15 16:35:50 +0100

    implemented horizontal grid lines

--- src/ch/tichuana/tichu/client/view/PlayArea.java
+++ src/ch/tichuana/tichu/client/view/PlayArea.java
@@ -2,7 +2,12 @@ package ch.tichuana.tichu.client.view;
 
 import ch.tichuana.tichu.client.services.ServiceLocator;
 import ch.tichuana.tichu.client.services.Translator;
+import javafx.geometry.HPos;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 
@@ -25,9 +30,11 @@ class PlayArea extends GridPane {
 		headings[6] = new Label(translator.getString("total"));
 
 		for (int i = 0; i < headings.length; i++)	{
-			this.add(headings[i], i+0, 0);
+			this.add(headings[i], i, 0);
 			GridPane.setHgrow(headings[i], Priority.ALWAYS);
 		}
+
+		this.add(new Separator(), 0, 1, 7, 1);
 
 		Label[] playerLbl = new Label[4];
 		playerLbl[0] = new Label("Philipp");
@@ -35,11 +42,23 @@ class PlayArea extends GridPane {
 		playerLbl[2] = new Label("Domi");
 		playerLbl[3] = new Label("Digi");
 
-		for (int i = 0; i < playerLbl.length; i++)	{
-			this.add(playerLbl[i], 0, i+1, 6, 1);
-			playerLbl[i].getStyleClass().add("playerLbl");
-			GridPane.setVgrow(playerLbl[i], Priority.ALWAYS);
-		}
+		this.add(playerLbl[0], 0, 2, 1, 1);
+		GridPane.setVgrow(playerLbl[0], Priority.ALWAYS);
+		this.add(new Separator(), 0, 3, 5, 1);
+
+
+		this.add(playerLbl[1], 0, 4, 1, 1);
+		GridPane.setVgrow(playerLbl[1], Priority.ALWAYS);
+		this.add(new Separator(), 0, 5, 7, 1);
+
+		this.add(playerLbl[2], 0, 6, 1, 1);
+		GridPane.setVgrow(playerLbl[2], Priority.ALWAYS);
+		this.add(new Separator(), 0, 7, 5, 1);
+
+		this.add(playerLbl[3], 0, 8, 1, 1);
+		GridPane.setVgrow(playerLbl[3], Priority.ALWAYS);
+
+		this.add(new Separator(), 0, 9, 9, 1);
 
 		this.setGridLinesVisible(true);
 		this.maxWidth(6000);
