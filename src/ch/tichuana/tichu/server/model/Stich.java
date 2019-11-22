a2d42c040a0f8442e5b99dec603fcc75036ac854
sekthor <sysdroyed@gmail.com>
2019-11-22 08:11:24 +0100

    fixed NullPointerException in Stich

--- src/ch/tichuana/tichu/server/model/Stich.java
+++ src/ch/tichuana/tichu/server/model/Stich.java
@@ -17,6 +17,7 @@ public class Stich {
     private ServerModel serverModel;
 
     public Stich(ServerModel serverModel){
+        this.cards =  new ArrayList<Card>();
         this.lastMove = new ArrayList<Card>();
         this.serverModel = serverModel;
         passCounter = 0;
